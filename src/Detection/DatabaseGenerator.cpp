ab357a51c024044b52bb30f9280af06a1a94bb08
Mato <mato@mato-laptop.(none)>
2013-11-05 18:24:01 +0100

    DatabaseGenerator

--- src/Detection/DatabaseGenerator.cpp
+++ src/Detection/DatabaseGenerator.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <sstream>
 #include <fstream>
 #include <boost/filesystem.hpp>
 #include <boost/foreach.hpp>
@@ -18,22 +19,83 @@ void loggerInit(char* argv0) {
 int main(int argc, char* argv[]) {
     loggerInit(argv[0]);
 
-    string filename("data");
-    path dataFilePath(filename);
-    if (exists(dataFilePath))
-        remove(filename);
+    FrameCapturer fc("192.168.50.81",80,"demo","demo");
+    fc.setZoom(2000);
+    FrameProcessor fp(fc);
 
-    ofstream dataFile(filename);
-    path dir(".");
-    directory_iterator it(dir), eod;
-    if(is_directory(dir)) {
-        BOOST_FOREACH(path const &p, std::make_pair(it, eod)) {
-            std::cout << p.filename().string() << std::endl;
-            dataFile << p.filename().string() << endl;
+    string x_filename("xData");
+    string y_filename("yData");
+    path x_dataFilePath(x_filename);
+    path y_dataFilePath(y_filename);
+    if (exists(x_dataFilePath))
+        remove(x_filename);
+    if (exists(y_dataFilePath))
+        remove(y_filename);
+
+    ofstream x_dataFile(x_filename);
+    ofstream y_dataFile(y_filename);
+    stringstream ss;
+    string x, y, wConfirm;
+    double pan, tilt, zoom;
+    while(wConfirm != "quit") {
+        wConfirm = "no";
+        cout << "X: ";
+        cin >> x;
+        cout << "Y: ";
+        cin >> y;
+        //cout << "X: " << x << endl;
+        //cout << "Y: " << y << endl;
+        while(wConfirm != "yes" && wConfirm != "quit") {
+            cout << "Ready to write data?(yes/no/quit): ";
+            cin >> wConfirm;
+        }
+        if (wConfirm == "quit")
+            break;
+
+        if (wConfirm == "yes"){
+            fc.getPanTiltZoom(pan,tilt,zoom);
+            x_dataFile << x << " ";
+            x_dataFile << "1:" << pan << " ";
+            x_dataFile << "2:" << tilt << " ";
+            x_dataFile << endl;
+
+            y_dataFile << y << " ";
+            y_dataFile << "1:" << pan << " ";
+            y_dataFile << "2:" << tilt << " ";
+            y_dataFile << endl;
+
+            ss << "X" << x << ":";
+            ss << "Y" << y << ":";
+            ss << "P" << pan << ":";
+            ss << "T" << tilt << ":";
+            ss << "Z" << tilt << ":.jpg";
+            fp.writeFrame(ss.str());
+            //Clear ss
+            ss.str(string());
+
+            cout << "X:" << x << " ";
+            cout << "Y:" << y << " ";
+            cout << "Pan:" << pan << " ";
+            cout << "Tilt:" << tilt << " ";
+            cout << "Zoom:" << zoom << " ";
+            cout << endl;
+            cout << endl;
         }
     }
 
-    dataFile.close();
+    x_dataFile.close();
+    y_dataFile.close();
+
+    //path dir(".");
+    //directory_iterator it(dir), eod;
+    //if(is_directory(dir)) {
+        //BOOST_FOREACH(path const &p, std::make_pair(it, eod)) {
+            //std::cout << p.filename().string() << std::endl;
+            //dataFile << p.filename().string() << endl;
+        //}
+    //}
+
+    //dataFile.close();
 
     return 0;
 }
